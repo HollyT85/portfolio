a8ee0cc386993cbbaba8b92b7e44b172608c2a92
HollyT85 <h.e.turner@gmail.com>
2024-04-11 18:57:33 +0000

    added success/fail pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
-let contact = document.getElementsByClassName('contact')
-let success = document.getElementsByClassName('success')
-let fail = document.getElementsByClassName('fail')
-let contactForm = document.getElementById('contact-form')
+let contact = document.getElementById('contact');
+let success = document.getElementById('success');
+let fail = document.getElementById('fail');
+let contactForm = document.getElementById('contact-form');
+let heading = document.getElementById('contact-heading');
 
 function sendEmail(contactForm) {
-    emailjs.send("service_o8a8nba", "template_3def7nx", {
-        "from_name": contactForm.name.value,
-        "to_name": "Holly",
-        "user_email": contactForm.email.value,
-        "message": contactForm.message.value,
+    emailjs.send('service_o8a8nba', 'template_3def7nx', {
+        'from_name': contactForm.name.value,
+        'to_name': 'Holly',
+        'user_email': contactForm.email.value,
+        'message': contactForm.message.value,
     })
     .then (
         function(response) {
-            console.log("SUCCESS", response);
+            console.log(response)
+            success.classList.remove('hide');
+            contact.classList.add('hide'); 
+            heading.classList.add('hide');
         },
         function (error) {
-            console.log("ERROR", error);
+            console.log(error)
+            fail.classList.remove('hide');
+            contact.classList.add('hide'); 
+            heading.classList.add('hide');
         }
     ); return false;
 }
